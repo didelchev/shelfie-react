c6259f21bb4a48f7be0e7ade0c75704071ec64e1
Daniel Delchev <daniel.delchev@yahoo.com>
2025-09-30 18:43:02 +0300

    add books-api

---
+++ client/src/api/books-api.js
@@ -0,0 +1,10 @@
+import { api } from "./requester.js"
+
+
+const API_URL = "https://shelfie-book-app.onrender.com/catalog"
+
+export const getAllBooks = () => api.get(API_URL)
+
+export const getOneBook = (bookId) => api.get(`${API_URL}/${bookId}`)
+
+export const getLatestBooks = () => api.get(API_URL)
