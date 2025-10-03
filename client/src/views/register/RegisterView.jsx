4cbf0606009ab4b6a03a537df6e4a81e72cd5317
Daniel Delchev <daniel.delchev@yahoo.com>
2025-10-03 17:03:06 +0300

    implement basic register component logic

--- client/src/views/register/RegisterView.jsx
+++ client/src/views/register/RegisterView.jsx
@@ -1,21 +1,65 @@
-import React from 'react'
+import React, { useState } from 'react'
 import "./RegisterView.css"
+import { api } from '../../api/requester'
+import { register } from '../../api/auth-api'
 
 const RegisterView = () => {
+
+  const [authData, setAuthData] = useState({
+    email: '',
+    username: '',
+    password: '',
+    're-password': ''
+  })
+
+
+  const [error, setError]  = useState(null);
+
+
+  const changeHandler = (e) => {
+    setAuthData({...authData, [e.target.name]: e.target.value});
+  }
+
+  const registerHandler = async (e) => {
+
+      e.preventDefault()
+      
+    const { email, username, password, rePassword } = authData;
+
+    try {
+      const user = await register(email, username, password, rePassword);
+
+      console.log(user)
+
+    } catch (error) {
+
+      const errorMessage = error.message || 'An unknown error occured while trying to register !'
+
+      setError(errorMessage)
+    }
+
+  }
+
+
   return (
     <div className="register-grid-container">
       <div className="left-section">
-        <form className="register-form">
+        <form className="register-form" onSubmit={registerHandler}>
             <h2>Create an account</h2>
             <label htmlFor="email">Email</label>
-            <input type="text" id="email" name="email" />
+            <input type="text" id="email" name="email" value={authData.email} onChange={changeHandler} />
             <label htmlFor="username">Username</label>
-            <input type="text" id="username" name="username" />
+            <input type="text" id="username" name="username" value={authData.username} onChange={changeHandler} />
             <label htmlFor="password">Password</label>
-            <input type="password" id="password" name="password" />
+            <input type="password" id="password" name="password" value={authData.password} onChange={changeHandler}/>
             <label htmlFor="re-password">Repeat Password</label>
-            <input type="password" id="re-password" name="re-password" />
+            <input type="password" id="re-password" name="re-password" value={authData["re-password"]} onChange={changeHandler}/>
             <button className ='submit' type="submit">Register</button>
+            {error && (
+              <div style={{ color: 'red', border: '1px solid red', padding: '10px', marginTop: '10px', textAlign: "center" }}>
+                Login Failed: {error}
+              </div>
+            )}
             <p className="registered">
               Already have an account ? <a href="/login">Sign in</a>
             </p>
