3fc147597d63a1bdd878e1021477e5daa90342b1
Daniel Delchev <daniel.delchev@yahoo.com>
2025-10-08 18:51:52 +0300

    add get user credentials func

--- client/src/api/user-api.js
+++ client/src/api/user-api.js
@@ -8,4 +8,5 @@ const API_URL = "https://shelfie-book-app.onrender.com"
 export const getUserCredentials = () => api.get(`${API_URL}/profile`)
 
 
+
 export const editUserCredentials = (updatedUser ) => api.patch(`${API_URL}/profile`)
