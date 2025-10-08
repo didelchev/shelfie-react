3f900a47a6707dce372ae2fd30eed6aaeb469794
Daniel Delchev <daniel.delchev@yahoo.com>
2025-10-08 18:52:07 +0300

    add basic user profile implementation

--- client/src/views/profile/ProfileView.jsx
+++ client/src/views/profile/ProfileView.jsx
@@ -1,27 +1,86 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import "./ProfileView.css"
+import { getUserCredentials } from '../../api/user-api';
+import ProfileBookTemplate from '../../components/profile-book/ProfileBookTemplate';
+import { getOneBook } from '../../api/books-api';
+import BookTemplate from '../../components/book/BookTemplate';
 
 const ProfileView = () => {
 
     //Functionalities: 
-    // 1) show user credentials
-    // 2) show user library
-    // 3) edit user info
+    // 1) [x] show user credentials 
+    // 2) [] show user library
+    // 3) [] edit user info
+
+    const [userCredentials, setUserCredentials] = useState({});
+
+    const [userShelves, setUserShelves ] = useState({
+        read: [],
+        currReading: [],
+        toRead: [],
+    })
+
+    useEffect(() => {
+        (async () => {
+            const user = await getUserCredentials();
+
+            setUserCredentials(user)
+
+
+              const [readBooks, currReadingBooks, toReadBooks] = await Promise.all([
+                    fetchBooksForShelf(user.read),
+                    fetchBooksForShelf(user.currReading),
+                    fetchBooksForShelf(user.toRead),
+                ]);
+
+
+
+            setUserShelves({
+                read: readBooks,
+                currReading: currReadingBooks,
+                toRead: toReadBooks
+            })
+
+        })()
+
+    },[])
+
+    const renderBook = (bookArray) => {
+       return  bookArray.map(book => <ProfileBookTemplate book = {book} key={book._id}/>)
+        }
+
+    const fetchBooksForShelf = async (bookShelf) => {
+
+        const stringIds = bookShelf.map(item => item);
+
+        const bookPromise = stringIds.map(id => getOneBook(id));
+
+        const books = await Promise.all(bookPromise);
+
+        return books
+
+    }
+    
+
+
+
+
+
 
 
 
 
   return (
-    <section className="profile-page">
+<section className="profile-page">
   <div className="profile-banner"></div>
   <div className="profile-card">
-    <img src={null} alt="Profile Picture" />
+    <img src={userCredentials.profileImageUrl} alt="Profile Picture" />
     <div className="profile-details">
-      <h2>John Doe</h2>
-      <p className="email">jonhdoe@gmail.com</p>
-      <p className="bio">Hello its me !</p>
+      <h2>{userCredentials.username}</h2>
+      <p className="email">{userCredentials.email}</p>
+      <p className="bio"></p>
     </div>
-    <button className="edit-btn">Edit</button>
+    <button className="edit-btn">Edit Profile</button>
   </div>
   {/* ${editing ? html`
   <form className="edit-form" @submit=${onSaveEdit}>
@@ -44,10 +103,15 @@ const ProfileView = () => {
     <h3>Read</h3>
     <div className="shelf-row">
       {/* ${books.filter(b => b.status === 'read').map(b => profileBooksTemplate(b,removeHandler))} */}
+      {/* {userCredentials.read.map(book => <ProfileBookTemplate book={book}/>)} */}
+      {renderBook(userShelves.read)}
+      
     </div>
 
     <h3>Currently Reading</h3>
-    <div className="shelf-row">
+    <div className="shel
+    
+    f-row">
       {/* ${books.filter(b => b.status === 'currReading').map(b => profileBooksTemplate(b,removeHandler))} */}
     </div>
 
