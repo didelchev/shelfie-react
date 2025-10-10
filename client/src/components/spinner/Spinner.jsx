3d7b1aa1ecb9292fd609b66ad7da4bea9e27aa6f
Daniel Delchev <daniel.delchev@yahoo.com>
2025-10-10 23:31:32 +0300

    add spinner component

---
+++ client/src/components/spinner/Spinner.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import "./Spinner.css";
+
+export default function Loading() {
+  return (
+    <div className="loading-container">
+      <div className="lds-roller">
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+      </div>
+    </div>
+  );
+}
