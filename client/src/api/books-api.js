ac92eba2b21a0e5edf4f34e70ebe88d7c8415cb7
Daniel Delchev <daniel.delchev@yahoo.com>
2025-10-20 13:41:53 +0300

    add remove book service

--- client/src/api/books-api.js
+++ client/src/api/books-api.js
@@ -13,6 +13,8 @@ export const addBookReview = (bookId, review) => api.post(`${API_URL}/reviews/${
 
 export const addBookToShelf = (bookId, shelf) => api.post(`${API_URL}/catalog/${bookId}`, shelf)
 
+export const removeBookFromShelf = (bookId, shelf) => api.patch(`${API_URL}/catalog/${bookId}`, shelf)
+
 export const getBookReviews = (bookId) => api.get(`${API_URL}/reviews/${bookId}`)
 
 export const addBookRating = (bookId, rating) => api.post(`${API_URL}/reviews/${bookId}/ratings`, rating)
