eb4db32cffb5804877fedcb60d4a0598099e0630
Daniel Delchev <daniel.delchev@yahoo.com>
2025-10-21 21:31:49 +0300

    update

--- client/src/components/profile-book/ProfileBookTemplate.jsx
+++ client/src/components/profile-book/ProfileBookTemplate.jsx
@@ -1,16 +1,19 @@
 import React from 'react'
 
-const ProfileBookTemplate = ({ book }) => {
+const ProfileBookTemplate = ({ book, onRemove }) => {
+
   return (
     <div className="single-book">
     <div className="image-wrapper">
       <a className="link" href="/catalog/${book._id}">
         <img src={book.image} alt={book.title} />
       </a>
-      {/* <button className="remove-btn" @click=${() => removeHandler(book._id, book.status)}>✖</button> */}
+      <button className="remove-btn" onClick={onRemove}>✖</button>
     </div>
   </div>
   )
 }
 
 export default ProfileBookTemplate
+
+
