956d6b14be849d5ca9b13d0a86fb962d08c893f0
Daniel Delchev <daniel.delchev@yahoo.com>
2025-10-23 05:13:53 +0300

    add dynamic nav links

--- client/src/components/burger/BurgerComponent.jsx
+++ client/src/components/burger/BurgerComponent.jsx
@@ -1,10 +1,13 @@
 import React, { useState } from "react";
 import Hamburger from "hamburger-react";
 import { Link } from "react-router-dom";
+import { useAuth } from "../../contexts/AuthContext";
 import './BurgerComponent.css'
 
 const BurgerComponent = () => {
   const [isOpen, setOpen] = useState(false);
+
+  const { isAuthenticated } = useAuth();
 
   const closeMenu = () => {
     setOpen(false); 
@@ -27,12 +30,24 @@ const BurgerComponent = () => {
 
       <nav className={`menu-panel ${isOpen ? 'visible' : ''}`}>
         <ul>
-          <li><Link to="/" onClick={closeMenu}>Home</Link></li>
-          <li><Link to="/discover" onClick={closeMenu}>Browse Books</Link></li>
-          <li><Link to="/login" onClick={closeMenu}>Login</Link></li>
-          <li><Link to="/register" onClick={closeMenu}>Register</Link></li>
-          <li><Link to="/profile" onClick={closeMenu}>My Profile</Link></li>
-          <li><Link to="/logout" onClick={closeMenu}>Logout</Link></li>
+            {isAuthenticated ? (
+                <div className="burger-user-links">
+                    <li><Link to="/" onClick={closeMenu}>Home</Link></li>
+                    <li><Link to="/catalog" onClick={closeMenu}>Browse Books</Link></li>
+                    <li><Link to="/profile" onClick={closeMenu}>My Profile</Link></li>
+                    <li><Link to="/logout" onClick={closeMenu}>Logout</Link></li>
+                </div>
+                
+
+            ) : (
+                <div className="burger-guest-links">
+                    <li><Link to="/" onClick={closeMenu}>Home</Link></li>
+                    <li><Link to="/catalog" onClick={closeMenu}>Browse Books</Link></li>
+                    <li><Link to="/login" onClick={closeMenu}>Login</Link></li>
+                    <li><Link to="/register" onClick={closeMenu}>Register</Link></li>
+                </div>
+
+            )}
         </ul>
       </nav>
     </div>
